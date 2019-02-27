ad3507f01cf357b4594cc4d4dcd3bef92cedc4bd
Shri Raghu Raaman <shriraghuraman@gmail.com>
2019-02-27 11:33:31 +0530

    Solved error in contact form and otp

--- components/home/head.js
+++ components/home/head.js
@@ -16,9 +16,9 @@ const Head = props => (
     />
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" sizes="192x192" href="/static/touch-icon.png" />
-    <link rel="apple-touch-icon" href="/static/touch-icon.png" />
-    <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
+    {/* <link rel="icon" sizes="192x192" href="/static/touch-icon.png" /> */}
+    {/* <link rel="apple-touch-icon" href="/static/touch-icon.png" /> */}
+    {/* <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" /> */}
     <link rel="icon" href="/static/favicon.ico" />
     <link
       href="https://fonts.googleapis.com/icon?family=Material+Icons"
@@ -37,7 +37,7 @@ const Head = props => (
     <meta property="og:image" content={props.ogImage || defaultOGImage} />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
-    <script src="https://checkout.razorpay.com/v1/checkout.js" />
+    {/* <script src="https://checkout.razorpay.com/v1/checkout.js" /> */}
   </NextHead>
 );
 
